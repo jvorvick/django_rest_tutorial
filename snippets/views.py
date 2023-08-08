8f2b18f3066f18697ab743720f5d3130d55bf1a5
jvorvick <vorvickjesse@gmail.com>
2023-08-07 17:58:20 -0700

    associating Snippets with Users

--- snippets/views.py
+++ snippets/views.py
@@ -24,6 +24,9 @@ class UserDetail(generics.RetrieveAPIView):
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 # class SnippetList(mixins.ListModelMixin,
 #                   mixins.CreateModelMixin,
