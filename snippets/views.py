0e8af00b24fc461d944e715b7749b610fdba1f82
jvorvick <vorvickjesse@gmail.com>
2023-08-10 16:50:30 -0700

    object level permissions

--- snippets/views.py
+++ snippets/views.py
@@ -13,6 +13,8 @@ from rest_framework import generics, permissions
 
 from django.contrib.auth.models import User
 
+from snippets.permissions import IsOwnerOrReadOnly
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -32,7 +34,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 # class SnippetList(mixins.ListModelMixin,
 #                   mixins.CreateModelMixin,
